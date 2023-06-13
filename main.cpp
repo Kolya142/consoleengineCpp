3b20f0d15eea026cd3d9c2668667e88c572d5486
Kolya <87275865+Kolya142@users.noreply.github.com>
2023-06-13 15:43:29 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,22 +2,17 @@
 #include <stdio.h>
 #include <Windows.h>
 #include "player.h"
+#include "GObject.h"
 #include "settings.h"
+#include "Brick.h"
 #include "Map.h"
 #include <chrono>
+#define abs(x) (x < 0) ? -x : x
 
-float getCurrentTimeInMilliseconds()
-{
-    std::chrono::high_resolution_clock::time_point now = std::chrono::high_resolution_clock::now();
-    std::chrono::nanoseconds duration = now.time_since_epoch();
-
-    // Convert the duration to milliseconds
-    std::chrono::milliseconds ms = std::chrono::duration_cast<std::chrono::milliseconds>(duration);
-
-    // Convert the milliseconds to a float
-    float timeMs = static_cast<float>(ms.count());
-
-    return timeMs;
+float getCurrentTimeInMilliseconds() {
+    auto currentTime = std::chrono::high_resolution_clock::now();
+    auto duration = currentTime.time_since_epoch();
+    return std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
 }
 
 
@@ -66,24 +61,30 @@ int main()
 {
     setConsoleSize();
     map.init();
-    int* p = map.load();
+    // int* p = map.load();
     hideCursor();
-    Player player = Player(p[0], p[1]);
-    float st;
+    Player player = Player(1, 1);
+    Brick bricks[5];
+    bricks[0] = Brick(45, 16, 40, 4);
+    bricks[1] = Brick(60, 12, 5, 4);
+    bricks[2] = Brick(93, 16, 40, 4);
     std::string black = "";
     for (int i = 0; i < width; i ++) black += " ";
     bool frist = true;
+    float fps = 0.0f;
     while (GetKeyState(VK_ESCAPE) >= 0) {
-        st = getCurrentTimeInMilliseconds();
+        float st = getCurrentTimeInMilliseconds();
         map.clear();
+        for (int i = 0; i < 3; i++)
+            bricks[i].update(&map, width, height-1);
         player.update(&map, width, height-1);
         draw(frist);
         setCursor(0, height-1);
         std::cout << black;
         setCursor(0, height-1);
-        std::cout << "fps: " << "not implemented";
+        std::cout << "fps: ~" << fps;
         if (frist) frist = !frist;
+        fps = abs(100000 / (getCurrentTimeInMilliseconds() - st));
     }
-    while(true);
     return 0;
 }
